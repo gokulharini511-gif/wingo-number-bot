c4393adb80c65f4581308235e34f2230e6be8b44
gokulharini511-gif <gokulharini511@gmail.com>
2026-08-07 15:15:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 10000;
 
 const BOT_TOKEN = '8950819463:AAGrZXE-tL39JbvBP9wkc9fDzRFsTxxWYUU';
 
-// உங்களுடைய Channel ID அல்லது Channel Public Username (எ.கா: '@mychannelname')
-const CHANNEL_ID = '-1003310985903'; 
+// Updated with your Public Channel Username
+const CHANNEL_ID = '@LIFEOFWINFREE'; 
 
 const REGISTER_LINK = 'https://www.rajastake7.com/#/register?invitationCode=172723872480';
 
@@ -258,7 +258,7 @@ async function fetchWinGoData() {
             lastSentPeriod = nextPeriod;
             lastPredictedPeriod = nextPeriod;
             lastPredictedNumbers = pred.targetNumbers;
-            console.log("[SUCCESS] Processed Period: " + nextPeriod);
+            console.log("[SUCCESS] Sent Prediction to " + CHANNEL_ID + " for Period: " + nextPeriod);
         }
     } catch (error) {
         console.error('[PROCESS ERROR]:', error.message);
