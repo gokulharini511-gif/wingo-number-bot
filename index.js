9b6a3a5d5360ffb81fc92de83ff38b218b7312b5
gokulharini511-gif <gokulharini511@gmail.com>
2026-08-08 09:53:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -298,4 +298,5 @@ async function fetchWinGoData() {
 process.on('uncaughtException', (err) => console.error('Uncaught Exception:', err));
 process.on('unhandledRejection', (reason, promise) => console.error('Unhandled Rejection:', reason));
 
-setInterval(fetchWinGoData, 3000);
+// 15 seconds delay (15000ms)
+setInterval(fetchWinGoData, 15000);
